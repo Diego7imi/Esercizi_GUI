c1c80af7d5f5c3cbd7a303eb8d94c542f57f8004
Diego Settimi <S1098119@studenti.univpm.it>
2021-12-05 17:50:32 +0100

    Fine

--- Grafica/src/Interfaccia.java
+++ Grafica/src/Interfaccia.java
@@ -4,7 +4,53 @@ public class Interfaccia {
 
 	public static void main(String[] args) 
 	{
-		
+		// l'oggetto contatore che vogliamo utilizzare
+				Contatore cont = new Contatore();
+				
+				// crea la finestra del menu principale
+				JFrame menu = new JFrame("Menu Principale");
+				// imposta la dimensione della finestra
+				menu.setSize(300, 100);
+				// specifica che la chiusura della finestra deve
+				// fare terminare l'intero programma
+				menu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				
+				// primo bottone (mostra il valore del contatore)
+				JButton button_mostra = new JButton("Mostra");
+				// associa al bottone il suo comportamento
+				button_mostra.addActionListener(new ButtonMostra(cont));
+				
+				// secondo bottone (incrementa di 1)
+				JButton button_incrementa = new JButton("Incrementa");
+				// associa al bottone il suo comportamento
+				button_incrementa.addActionListener(new ButtonIncrementa(cont));
+
+				// terzo bottone (decrementa di 1)
+				JButton button_decrementa = new JButton("Decrementa");
+				// associa al bottone il suo comportamento
+				button_decrementa.addActionListener(new ButtonDecrementa(cont));
+				
+				// quarto bottone (azzera il contatore)
+				JButton button_azzera = new JButton("Azzera");
+				// associa al bottone il suo comportamento
+				button_azzera.addActionListener(new ButtonAzzera(cont));
+
+				// un panel e' un contenitore di elementi da
+				// visualizzare in una finestra
+				JPanel panel = new JPanel();
+				
+				// aggiungiamo tutti i bottoni al panel
+				// nell'ordine in cui vogliamo siano visualizzati
+				panel.add(button_mostra);
+				panel.add(button_incrementa);
+				panel.add(button_decrementa);
+				panel.add(button_azzera);
+				
+				// infine aggiungiamo il panel alla finestra
+				menu.add(panel);
+				
+				// visualizza la finestra
+				menu.setVisible(true);
 	}
 
 }
